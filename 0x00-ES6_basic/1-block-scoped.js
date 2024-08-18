767ead43b94f2a6c2c24bea2657d1e6c86cf1131
Ngwere <ngwerecliford@gmail.com>
2024-08-17 17:45:43 -0700

    Oohpps task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,7 +1,7 @@
-/* eslint-disable no-unused-vars */	
+/* eslint-disable no-unused-vars */
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
     const task = true;
